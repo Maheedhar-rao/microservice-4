e3c8a9adcddc864e0e5fb405af2de1caade59633
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-05-29 13:05:13 -0400

    Update server.js
    changes in dealid

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/api/live-replies', async (req, res) => {
 
 app.get('/api/manual-reply-search', async (req, res) => {
   const { dealid, business_name } = req.body;
-  if (!deal_id || !business_name) return res.status(400).json({ message: 'Missing fields' });
+  if (!dealid || !business_name) return res.status(400).json({ message: 'Missing fields' });
 
   const { data: deals, error } = await supabase
     .from('deals_submitted')
