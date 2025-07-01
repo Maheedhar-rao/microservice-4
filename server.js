4a284951a67f60dcd821b2d1bfdb488a752bb2cf
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-06-30 23:08:12 -0400

    Update server.js
    added const decoded = jwt.verify(token, process.env.JWT_SECRET);

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function verifyUser(req, res, next) {
   if (!token) return res.redirect('https://login.croccrm.com');
 
   try {
-    jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; 
     next();
   } catch {
